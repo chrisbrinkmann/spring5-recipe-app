2b992409cd72402e0e08b7476522be2f85707171
Chris Brinkmann <chris.brinkmann@us-dde.net>
2021-01-20 11:30:56 -0500

    Add repositories and do some queries in controller

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -15,8 +15,22 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("American Category ID is: " + categoryOptional.get().getId());
+        System.out.println("Teaspoon UOM ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,5 +1,6 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,5 +10,5 @@ import java.util.Optional;
  * Created by jt on 6/13/17.
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
-
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
